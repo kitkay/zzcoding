43a87d6cace31313020e6db33ed94094fccf6e01
Marvin Abante <abante.marvinkaye@gmail.com>
2021-09-15 23:40:55 +0800

    Level One Test File

---
+++ test/test_palindrome.py
@@ -0,0 +1,42 @@
+#Import unit test built-in function
+import unittest
+
+#Import file to be test.
+from palindromeChecker import Palindrome
+
+class TestStringPalindrome(unittest.TestCase):
+
+    # Make a static string to test and call sanitize method as global
+    stringToCheck = "!@#$%^amazinGnizam.<>?()_a"
+    sanitizedString = Palindrome.sanitizeData(stringToCheck)
+
+    #Checking if the global sanitization succeeded.
+    def test_sanitizeData(self):
+        try:
+            self.assertTrue(self.sanitizedString)
+            print("Sanitize Data Passed!")
+        except:
+            print("Sanitize Data Failed!")
+
+    #Match sanitized data to indicated string
+    def test_sanitizeMatch(self):
+        try:
+            self.assertEqual(self.sanitizedString, 'amazingnizama')
+            print("Sanitize \""+self.stringToCheck+"\" to "+self.sanitizedString+" Passed!")
+        except:
+            print('Sanitized data did not match indicated string')
+
+    # Check if the sanitized data is a palindrome or not
+    def test_isPalindrome(self):
+        # palCheck = self.getString.isPalindrome()
+        palCheck = Palindrome.isPalindrome(self.sanitizedString)
+        try:
+            self.assertTrue(palCheck)
+            print("Palindrome string passed!")
+        except:
+            print("Palindrome string failed!")
+
+    # def test_checkPalindrome(self
+
+if __name__ == '__main__':
+    unittest.main()
